4b71faa7229c060c4ed18e7e05d4c061fbd6d0e5
Alice Vilar SimÃµes <14302889659@dc01.ifs.ifsuldeminas.edu.br>
2022-06-29 16:55:58 -0300

    front-controller

--- controller/Categoria.php
+++ controller/Categoria.php
@@ -1,9 +1,31 @@
 <?php
-require "../model/CategoriaModel.php";
+require "model/CategoriaModel.php";
 
-$categoria = new CategotiaModel();
-$categoria->inserir("Produtos Limpeza");
-$categoria->excluir(1);
-$categoria->atualizar(2, "SmartPhone");
-var_dump($categoria->buscarPorId(3));
-var_dump($categoria->buscarTudo());
+class Categoria{
+
+    function __construct() {
+        $this->model = new CategotiaModel();
+    }
+
+    function index(){
+        include "view/template/conteudo.php";
+    }
+
+    function add(){
+        echo "mostrar form categoria";
+    }
+
+    function excluir($id){
+        echo "excluir categoria";
+    }
+}
+
+
+
+
+//$categoria = new CategotiaModel();
+//$categoria->inserir("Produtos Limpeza");
+//$categoria->excluir(1);
+//$categoria->atualizar(2, "SmartPhone");
+//var_dump($categoria->buscarPorId(3));
+//var_dump($categoria->buscarTudo());
